e418b99b775e25db504a6b0cb1b7d9968678f611
Prathvi Raj Chauhan <prathvirajchauhandelhi6229@gmail.com>
2025-07-31 21:29:53 +0530

    Fixing CORS errors to convert it into a webapp also

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require("express")
 const mongoose = require("mongoose")
 const path = require("path")
+const cors = require("cors")
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => { console.log("mongodb connected successfully ") })
 .catch((e) => { console.log("error in conncecting to mongo db", e) })
@@ -13,6 +14,10 @@ const commentRoute = require("./routes/commentRoute")
 const app = express()
 const PORT = process.env.PORT || 8000;
 
+
+app.use(cors({
+  origin: 'http://localhost:52911', // only this localhost is allowed for now
+}));
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 app.use('/uploads', express.static(path.resolve('./public/uploads')))
